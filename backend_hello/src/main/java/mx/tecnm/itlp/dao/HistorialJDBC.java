806ee93ba41d8ec8f7e16b15bcc5aa6f73e00343
Alexis Julian <nant2dear@gmail.com>
2023-04-18 09:29:02 -0600

    FEAT: agregado endpoint historial
    agregar historial

---
+++ backend_hello/src/main/java/mx/tecnm/itlp/dao/HistorialJDBC.java
@@ -0,0 +1,25 @@
+package mx.tecnm.itlp.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import mx.tecnm.itlp.models.Historial;
+
+@Repository
+public class HistorialJDBC {
+    
+    @Autowired
+    JdbcTemplate conexion;
+
+    public void agregar(Historial historial){
+        String sql = "INSERT INTO historial (fecha, tiempo_avance, pelicula_id, perfil_id) "
+        +" VALUES (?, ?, ?, ?)";
+        conexion.update(sql, historial.getFecha(), 
+        historial.getTiempoAvance(), 
+        historial.getPeliculaId(), 
+        historial.getPerfilId());
+    }
+
+   
+}
